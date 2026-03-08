13c11ac7ce9de2bbe3e314987a4e10c66558180e
Avanish <avanishsamala@gmail.com>
2026-03-08 17:27:42 -0400

    Add Capacitor iOS project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.avanishsamala.empire',
+  appName: 'Empire',
+  webDir: 'dist'
+};
+
+export default config;
